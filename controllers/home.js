93f42304ca4b7c83ef1059a401b066c421d7fff9
Alex Timis <timis.alexioan@gmail.com>
2026-05-18 06:53:19 +0300

    added env in gitignore and created the homepage.ejs

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,8 @@
 module.exports = {
-    getIndex: (req,res)=>{
-        res.render('index.ejs')
-    }
-}
+  getIndex: (req, res) => {
+    res.render("index.ejs");
+  },
+  getHomepage: (req, res) => {
+    res.render("homepage.ejs");
+  },
+};

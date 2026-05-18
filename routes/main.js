9e585acfd1e72605fc8d5d5140c26b9696248d51
Alex Timis <timis.alexioan@gmail.com>
2026-05-18 19:33:09 +0300

    created restaurant requirements

--- routes/main.js
+++ routes/main.js
@@ -4,13 +4,11 @@ const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
-router.get("/", homeController.getIndex);
+router.get("/", homeController.getHomepage);
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
 router.get("/logout", authController.logout);
 router.get("/signup", authController.getSignup);
 router.post("/signup", authController.postSignup);
-
-router.get("/home", homeController.getHomepage);
 
 module.exports = router;

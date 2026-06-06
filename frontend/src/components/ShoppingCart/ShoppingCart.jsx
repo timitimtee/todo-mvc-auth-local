99a0a90b092e9701d38f9ae8f0d32a7a61cd2736
Alex Timis <timis.alexioan@gmail.com>
2026-06-06 16:08:46 +0300

    added a new icon to the shpping cart amde nav rsponsive

--- frontend/src/components/ShoppingCart/ShoppingCart.jsx
+++ frontend/src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,11 +1,17 @@
 import "./ShoppingCart.css";
+import shoppingCart from "./icons8-shopping-bag.png";
 
 export default function ShoppingCart({ count }) {
   return (
     <>
-      <div>
-        <img></img>
-        <span>${count}</span>
+      <div className="shopping-signin-container">
+        <div className="shopping-cart">
+          <img src={shoppingCart} alt="Shopping Bag" />
+          <span className="cartCount">{count}</span>
+        </div>
+        <div className="sign-in">
+          <span>Sign In</span>
+        </div>
       </div>
     </>
   );
